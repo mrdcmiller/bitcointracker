fe0c003f5b8d4c50b4c59c79c65d651da5a33cb6
Doug Miller <doug@cubicdigital.com>
2019-03-12 11:48:07 -0400

    Add files via upload

---
+++ BitcoinTicker/CoinDataModel.swift
@@ -0,0 +1,21 @@
+//
+//  CoinDataModel.swift
+//  BitcoinTicker
+//
+// Created by Doug Miller on 2/23/19.
+//  
+//
+
+import UIKit
+
+class CoinDataModel {
+    
+    //Declare model variables 
+    var ask : Float = 3176.33
+    var bid : Float = 3176.33
+    var last: Float = 3177.69
+    var high: Float = 3182.17
+    var low: Float = 3015.1
+    
+}
+
